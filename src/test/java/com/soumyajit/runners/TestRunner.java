fe7acc913e664402d879cd898167994f2e15e5c2
TCS-Employee <nhidungare@gmail.com>
2026-02-09 08:51:49 +0000

    Working till seat selection and returning available seats

--- src/test/java/com/soumyajit/runners/TestRunner.java
+++ src/test/java/com/soumyajit/runners/TestRunner.java
@@ -1,11 +1,12 @@
 package com.soumyajit.runners;
+//package com.soumyajit.stepdefinitions;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"soumyajit.stepdefinitions", "soumyajit.hooks"},
+        glue = {"com.soumyajit.stepdefinitions", "com.soumyajit.hooks"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-report.html"
